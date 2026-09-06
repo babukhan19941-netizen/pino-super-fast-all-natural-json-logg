cafa713c77b4dca4a4f21fa7ae6c55aca0ba5878
babukhan19941-netizen <babukhan19941@gmail.com>
2026-09-06 22:09:21 +0530

    feat: Initialize Android app with Gradle 9.3.1 from HTML

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pino - Super fast, all natural JSON logg"
+include(":app")
